259ddff4580f8cbe45a9280b58c9831127486e1f
Taylor Monacelli <taylor.monacelli@streambox.com>
2016-11-12 19:14:17 -0800

    Revert "Upgrade guest virtualbox addtions"
    Reverting 20973dae16495ac4cd40ad0642b5f2016e3e2136 cuz it seems to have
    no effect.
    
    This reverts commit 20973dae16495ac4cd40ad0642b5f2016e3e2136.

--- VagrantAdditionalConfig.rb
+++
@@ -1,8 +0,0 @@
-VAGRANTFILE_API_VERSION = "2"
-
-Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-  # Upgrade guest additions to get around cygwin setup fails
-  if Vagrant.has_plugin?('vagrant-vbguest')
-    config.vbguest.auto_update = true
-  end
-end
